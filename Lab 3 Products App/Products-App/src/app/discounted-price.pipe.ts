273cd4aea166135b0bd72ecbbe9bf1eaf15827a9
esraakhalifa <esraakhalifa5@gmail.com>
2025-01-20 21:33:37 +0200

    added the custom pipe in lab 3

--- Lab 3 Products App/Products-App/src/app/discounted-price.pipe.ts
+++ Lab 3 Products App/Products-App/src/app/discounted-price.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DiscountedPricePipe implements PipeTransform {
 
-  transform(price:number, discount:number): unknown {
-    return price * (discount/100);
+  transform(price:number, discount:number = 1): unknown {
+    if (!price || !discount) return price;  
+    return (price - (price * discount / 100)).toFixed(2);
   }
 
 }
